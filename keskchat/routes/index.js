4464e04d18a915eaccfc72daa2ea67f80cd7b82b
ElsaaTan <113075566+ElsaaTan@users.noreply.github.com>
2023-07-26 18:28:16 -0400

    Added home page

--- keskchat/routes/index.js
+++ keskchat/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Kesk Chat' });
+});
+
+/* GET chat room page. */
+router.get('/chat', function(req, res, next) {
+  res.render('index', { title: 'Chat Room 1' });
 });
 
 module.exports = router;
