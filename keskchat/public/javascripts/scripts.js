ffc1511dd559210eda44da18417d781771ff9efc
kevyndowner <200488106@student.georgianc.on.ca>
2023-07-26 15:34:55 -0400

    added user is typing

--- keskchat/public/javascripts/scripts.js
+++ keskchat/public/javascripts/scripts.js
@@ -11,6 +11,7 @@ const messageContainer = document.getElementById('message-container');
 const nameInput = document.getElementById('name-input');
 const messageForm = document.getElementById('message-form');
 const messageInput = document.getElementById('message-input');
+const messageName = document.getElementById('message-name');
 
 
 //creating an event on the submit button
@@ -24,18 +25,24 @@ messageForm.addEventListener('submit', (event) =>
 //this function will send a message when the send button is pressed
 function sendMessage()
 {
-    //creating message package
-    console.log(messageInput.value);
-    var data = 
+    //preventing empty messages
+    if(messageInput.value === ''){}
+    else
     {
-        name: nameInput.value,
-        message: messageInput.value
+        //creating message package
+        console.log(messageInput.value);
+        var data = 
+        {
+            name: nameInput.value,
+            message: messageInput.value
+        }
+    
+        //this is needed for detecting messages
+        socket.emit('message', data)
+        addMessage(true, data);
+        messageInput.value = "";
     }
     
-    //this is needed for detecting messages
-    socket.emit('message', data)
-    addMessage(true, data);
-    messageInput.value = "";
 }
 
 //will call the addMessage on message recieved
@@ -47,17 +54,56 @@ socket.on('chat-message', (data) =>
 //this function will handle the messages
 function addMessage(isSender, data)
 {
+    clearUserTyping();
     //determine which side of the message box it ends up on
     if(isSender)
     {
-        var element = '<li class="message-right"><p class="message">' + data.name + "-" + data.message + '</p></li>'; 
+        var element = '<li class="message-right"><p class="message-name">' + data.name + '</p><p class="message">' + data.message + '</p></li>'; 
     } 
     else
     {
-        var element = '<li class="message-left"><p class="message">' + data.name + "-" + data.message + '</p></li>'; 
+        var element = '<li class="message-left"><p class="message-name">' + data.name + '</p><p class="message">' + data.message + '</p></li>';
     } 
     
-    //adding element
+    //adding elements
     messageContainer.innerHTML += element;
 
+}
+
+//Modified design for user is typing from: https://www.youtube.com/watch?v=gZvpCiPIzW0&list=PLdHg5T0SNpN09AlLBAYahKZUrAWsIL7No&index=7
+
+//adding an event listener to the input field to detect if the user is typing
+messageInput.addEventListener('focus', (event)=>{
+    socket.emit('feedback', {
+        feedback: nameInput.value + " is typing a message...",
+    })
+});
+
+messageInput.addEventListener('click', (event)=>{
+    socket.emit('feedback', {
+        feedback: nameInput.value + " is typing a message...",
+    })
+});
+
+
+//adding an event listener to the input field when the user un focuses
+messageInput.addEventListener('blur', (event)=>{
+    socket.emit('feedback', {
+        feedback: "",
+    })
+});
+
+//recieving the feeback message
+socket.on('feedback', (data) =>
+{
+    clearUserTyping();
+    var element = '<li class="user-typing"><p>' + data.feedback + '</p></li>';
+    messageContainer.innerHTML += element;
+});
+
+//this function will remove all user typing
+function clearUserTyping(){
+    document.querySelectorAll('li.user-typing').forEach(element => {
+        element.parentNode.removeChild(element);
+    });
 }
